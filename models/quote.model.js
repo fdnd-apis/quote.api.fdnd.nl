bd06aadb7561135761d942b34f4db34bf11c0cf4
ju5tu5 <justus@ju5tu5.nl>
2022-01-26 17:39:25 +0100

    Added a quote model

--- models/quote.model.js
+++ models/quote.model.js
@@ -2,10 +2,29 @@ const db = require('./db')
 const helper = require('./helper')
 
 /**
- *
- * @param {*} quote
+ * Constructor for new quotes that checks if the passed object adheres the format
+ * we need and throws errors if it doesn't
+ * @param {*} quote an object containing the necessary fields to make a new quote
  */
 const Quote = function (quote) {
   // TODO: Check for sanity...
   this.quoteId = quote.quoteId
+}
+
+/**
+ * Get all quotes from the database, will be paginated if the number of
+ * quotes in the database exceeds process.env.LIST_PER_PAGE
+ * @param {*} page the page of quotes you want to get
+ * @returns
+ */
+Quote.getAll = async function (page = 1) {
+  const rows = await db.query(`SELECT * FROM quote LIMIT ?,?`, [
+    helper.getOffset(page, process.env.LIST_PER_PAGE),
+    process.env.LIST_PER_PAGE,
+  ])
+
+  return {
+    data: helper.emptyOrRows(rows),
+    meta: { page },
+  }
 }
---
+++ routes/quote.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const Tribe = require('../models/quote.model')
+
+module.exports = express
+  .Router()
+
+  // List ALL quotes
+  .get('/', async (req, res, next) => {
+    try {
+      res.json(await Quote.getAll(req.query.page))
+    } catch (err) {
+      console.error('Error while getting tribes: ', err.message)
+      next(err)
+    }
+  })
