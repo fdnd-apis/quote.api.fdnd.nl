1c4018637fe1ef1b619be8eb06377893ea7ac2e3
ju5tu5 <justus@ju5tu5.nl>
2022-01-27 20:58:29 +0100

    Update quote.model.js

--- models/quote.model.js
+++ models/quote.model.js
@@ -9,6 +9,8 @@ const helper = require('./helper')
 const Quote = function (quote) {
   // TODO: Check for sanity...
   this.quoteId = quote.quoteId
+  this.authorId = quote.authorId
+  this.text = quote.text
 }
 
 /**
@@ -19,7 +21,7 @@ const Quote = function (quote) {
  */
 Quote.getAll = async function (page = 1) {
   const rows = await db.query(
-    `SELECT q.quoteId, q.text, a.authorId, a.name, a.bio, a.avatar FROM quote as q LEFT JOIN author as a ON q.authorId = a.authorId LIMIT ?,?`,
+    `SELECT q.quoteId, q.tags, q.text, a.authorId, a.name, a.bio, a.avatar FROM quote as q LEFT JOIN author as a ON q.authorId = a.authorId LIMIT ?,?`,
     [helper.getOffset(page, process.env.LIST_PER_PAGE), process.env.LIST_PER_PAGE]
   )
 
