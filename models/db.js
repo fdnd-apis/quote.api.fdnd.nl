fd0ea3c15624773b7295f44861c27d948a6140db
ju5tu5 <justus@ju5tu5.nl>
2022-02-14 15:12:20 +0100

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,4 +1,14 @@
-const mysql = require('mysql2/promise')
+const mysql = require('mysql2')
+
+const pool = mysql.createPool({
+  connectionLimit: 200,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+})
+
+const promisePool = pool.promise()
 
 /**
  * Async function that makes a connection to the db and executes the query with
@@ -8,12 +18,7 @@ const mysql = require('mysql2/promise')
  * @param {*} params - the parameters used to fill in ?'s in the sequel string
  */
 module.exports.query = async function (sql, params) {
-  const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-  })
-  const [rows] = await connection.execute(sql, params)
+  console.log(sql, params)
+  const [rows] = await promisePool.query(sql, params)
   return rows
 }
